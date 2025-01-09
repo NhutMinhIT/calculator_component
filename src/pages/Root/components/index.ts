82e03fac302552ddad5b985663e97486ae2bd9a6
NhutMinhIT <nhutminh.it19@gmail.com>
2025-01-09 16:21:12 +0700

    feat: update package names and dependencies, enhance Root component with customized text field

--- src/pages/Root/components/index.ts
+++ src/pages/Root/components/index.ts
@@ -1,2 +1,6 @@
 export { default as Transition } from './Transition/TransitionDialog';
 export { default as CalendarDialog } from './Dialog/CalendarDialog';
+
+//customize
+export { default as CustomizedTextField } from './Customize/CutomizeTextField';
+export { default as CustomizeCalendar } from './Customize/CustomizeCalendar';
--- src/pages/Root/index.tsx
+++ src/pages/Root/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import CalendarDialog from './components/Dialog/CalendarDialog';
 import { useCalendar } from './hook/useCalendar';
-import { TextField } from '@mui/material';
+import { Box } from '@mui/material';
 import { CALENDAR_TEXT_FIELD_LABEL, CALENDER_TEXT_FIELD_DATA_TEST_ID, CALENDER_TEXT_FIELD_ID } from './constant';
 import { convertDateToString } from './utils';
+import styles from './styles/style.module.css'
+import { CustomizedTextField } from './components';
 
 const Root = (): JSX.Element => {
     const { isOpenCalendarDialog, handleOpenCalendarDialog, handleCloseCalendarDialog } = useCalendar()
 
     return (
-        <>
-            <TextField
+        <Box className={styles.rootPage}>
+            <CustomizedTextField
                 label={CALENDAR_TEXT_FIELD_LABEL}
                 id={CALENDER_TEXT_FIELD_ID}
                 data-testid={CALENDER_TEXT_FIELD_DATA_TEST_ID}
@@ -23,7 +25,7 @@ const Root = (): JSX.Element => {
                 onClose={handleCloseCalendarDialog}
             />
 
-        </>
+        </Box>
     )
 }
 
