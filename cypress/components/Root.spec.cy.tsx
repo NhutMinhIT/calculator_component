e75bc9e978555cc4e7665aae1d596a549aaa02b5
NhutMinhIT <nhutminh.it19@gmail.com>
2025-01-10 16:44:14 +0700

    test: add Cypress tests for CalendarDialog and update Root component tests

--- cypress/components/Root.spec.cy.tsx
+++ cypress/components/Root.spec.cy.tsx
@@ -2,11 +2,12 @@ import { mount } from "cypress/react";
 import Root from "../../src/pages/Root";
 import { CALENDER_TEXT_FIELD_DATA_TEST_ID, ROOT_COMPONENT_DATA_TEST_ID } from "../../src/pages/Root/constant";
 
-const mountRoot = () => {
-    mount(<Root />);
-}
 
 describe('Root component', () => {
+    const mountRoot = () => {
+        mount(<Root />);
+    }
+
     it('should render root component successfully', () => {
         mountRoot();
         cy.get(`[data-testid=${ROOT_COMPONENT_DATA_TEST_ID}]`).should('exist');
