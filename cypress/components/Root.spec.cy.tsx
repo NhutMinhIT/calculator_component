617ce78fb7b4f4e45be96283598cf16216a09ef6
NhutMinhIT <nhutminh.it19@gmail.com>
2025-01-13 13:17:48 +0700

    test: add date selection test in Root component

--- cypress/components/Root.spec.cy.tsx
+++ cypress/components/Root.spec.cy.tsx
@@ -2,11 +2,11 @@ import { mount } from "cypress/react";
 import Root from "../../src/pages/Root";
 import {
     CALENDAR_DIALOG_CANCEL_BUTTON_TEST_ID,
-    CALENDAR_DIALOG_CANCEL_BUTTON_TEXT,
     CALENDAR_DIALOG_TEST_ID,
     CALENDER_TEXT_FIELD_DATA_TEST_ID,
     ROOT_COMPONENT_DATA_TEST_ID
 } from "../../src/pages/Root/constant";
+import dayjs from "dayjs";
 
 describe('Root component', () => {
     const mountRoot = () => {
@@ -40,5 +40,12 @@ describe('Root component', () => {
         cy.get(`[data-testid=${CALENDAR_DIALOG_CANCEL_BUTTON_TEST_ID}]`).click();
     })
 
-
+    //choose date
+    it('choose date successfully', () => {
+        mountRoot();
+        const Date = dayjs('2025-11-27').toDate();
+        cy.get(`[data-testid=${CALENDER_TEXT_FIELD_DATA_TEST_ID}]`).click();
+        cy.get(`[data-testid=${CALENDAR_DIALOG_TEST_ID}]`).should('exist')
+        cy.get(`[data-testid=${CALENDAR_DIALOG_TEST_ID}]`).find('button').contains(Date.getDate()).click();
+    })
 });
