46b99a14071d5b4ddeac06b0b3e79da655053572
NhutMinhIT <nhutminh.it19@gmail.com>
2025-01-13 11:30:19 +0700

    test: add tests for opening and closing dialog component in Root

--- cypress/components/Root.spec.cy.tsx
+++ cypress/components/Root.spec.cy.tsx
@@ -1,13 +1,14 @@
 import { mount } from "cypress/react";
 import Root from "../../src/pages/Root";
 import {
+    CALENDAR_DIALOG_CANCEL_BUTTON_TEST_ID,
+    CALENDAR_DIALOG_CANCEL_BUTTON_TEXT,
     CALENDAR_DIALOG_TEST_ID,
     CALENDER_TEXT_FIELD_DATA_TEST_ID,
     ROOT_COMPONENT_DATA_TEST_ID
 } from "../../src/pages/Root/constant";
 
 describe('Root component', () => {
-
     const mountRoot = () => {
         mount(<Root />);
     }
@@ -24,5 +25,20 @@ describe('Root component', () => {
         mountRoot();
         cy.get(`[data-testid=${CALENDAR_DIALOG_TEST_ID}]`).should('exist')
     })
+
+    //open/close dialog component
+    it('should open dialog component successfully', () => {
+        mountRoot();
+        cy.get(`[data-testid=${CALENDER_TEXT_FIELD_DATA_TEST_ID}]`).click();
+        cy.get(`[data-testid=${CALENDAR_DIALOG_TEST_ID}]`).should('exist')
+    })
+
+    it('should close dialog component successfully', () => {
+        mountRoot();
+        cy.get(`[data-testid=${CALENDER_TEXT_FIELD_DATA_TEST_ID}]`).click();
+        cy.get(`[data-testid=${CALENDAR_DIALOG_TEST_ID}]`).should('exist')
+        cy.get(`[data-testid=${CALENDAR_DIALOG_CANCEL_BUTTON_TEST_ID}]`).click();
+    })
+
 
 });
