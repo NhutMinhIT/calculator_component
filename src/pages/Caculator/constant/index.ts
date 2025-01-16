6fa8221a1dd87d42129ee7a0a5143d00da7d45a9
NhutMinhIT <nhutminh.it19@gmail.com>
2025-01-16 09:56:44 +0700

    refactor: implement Caculator page layout and styling with input field enhancements

---
+++ src/pages/Caculator/constant/index.ts
@@ -0,0 +1,4 @@
+export const CACULATOR_PAGE_DATA_TEST_ID: string = "caculatorPage";
+export const CACULATOR_TEXT_FIELD_LABEL: string = "Caculator";
+export const CACULATOR_TEXT_FIELD_ID: string = "caculatorInput";
+export const CACULATOR_TEXT_FIELD_DATA_TEST_ID: string = "caculatorInput";
--- src/pages/Caculator/index.tsx
+++ src/pages/Caculator/index.tsx
@@ -1,10 +1,53 @@
 import React from 'react'
+import style from './style/caculator-page.module.css'
+import {
+    Box,
+    FormControl,
+    IconButton,
+    InputAdornment,
+    InputLabel,
+    OutlinedInput
+} from '@mui/material'
+import {
+    CACULATOR_PAGE_DATA_TEST_ID,
+    CACULATOR_TEXT_FIELD_DATA_TEST_ID,
+    CACULATOR_TEXT_FIELD_ID,
+    CACULATOR_TEXT_FIELD_LABEL
+} from './constant'
+import { CalculateOutlined } from '@mui/icons-material'
 
-const Caculator = () => {
+const renderEndAdornmentInputField = (): JSX.Element => {
     return (
-        <div>
+        <InputAdornment position="end">
+            <IconButton>
+                <CalculateOutlined />
+            </IconButton>
+        </InputAdornment>
+    )
+}
 
-        </div>
+const Caculator = (): JSX.Element => {
+    return (
+        <Box
+            className={style.caculatorPage}
+            data-testid={CACULATOR_PAGE_DATA_TEST_ID}
+        >
+            <FormControl>
+                <InputLabel htmlFor={CACULATOR_TEXT_FIELD_ID}>
+                    {CACULATOR_TEXT_FIELD_LABEL}
+                </InputLabel>
+                <OutlinedInput
+                    data-testid={CACULATOR_TEXT_FIELD_DATA_TEST_ID}
+                    id={CACULATOR_TEXT_FIELD_ID}
+                    label={CACULATOR_TEXT_FIELD_LABEL}
+                    // value={}
+                    // onClick={}
+                    endAdornment={
+                        renderEndAdornmentInputField()
+                    }
+                />
+            </FormControl>
+        </Box>
     )
 }
 
